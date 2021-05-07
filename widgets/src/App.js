7a0c80be7cb72c5c2cc96ae4abad583b102e8d23
Diego <soterg.uhdeia@gmail.com>
2021-05-07 14:28:59 -0500

    create dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,27 +1,49 @@
-import React from 'react';
-import Accordion from './components/Accordion';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
-    {
-        title: "What is React",
-        content: "React is a front end framework"
-    },
-    {
-        title: "Why use React",
-        content: "Because it is beautiful"
-    },
-    {
-        title: "How to use React",
-        content: "By creating compoonents"
-    }
-]
+  {
+    title: "What is React",
+    content: "React is a front end framework",
+  },
+  {
+    title: "Why use React",
+    content: "Because it is beautiful",
+  },
+  {
+    title: "How to use React",
+    content: "By creating compoonents",
+  },
+];
+
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+  {
+    label: "The shade of blue",
+    value: "blue",
+  },
+];
 
 const App = () => {
-    return (
-        <div>
-            <Accordion items= {items}/>
-        </div>
-    )
-}
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
+};
 
 export default App;
